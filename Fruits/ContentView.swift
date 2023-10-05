903b9701fe204af686ddf9f7e18f227cae322434
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-05 23:34:47 +0300

    Initial commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by mac on 5.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by mac on 5.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
