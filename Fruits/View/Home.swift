c350fcd2c9f8a44f04887bd697cf4fef868461a1
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-07 14:15:29 +0300

    Create Home view

---
+++ Fruits/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Fruits
+//
+//  Created by mac on 6.10.2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
