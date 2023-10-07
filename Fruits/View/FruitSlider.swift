dec8ddd87ca7f3482fb7d9446edebd138d8e5358
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-07 14:15:12 +0300

    Create FruitSlider view

---
+++ Fruits/View/FruitSlider.swift
@@ -0,0 +1,28 @@
+//
+//  FIrstPage.swift
+//  Fruits
+//
+//  Created by mac on 6.10.2023.
+//
+
+import SwiftUI
+
+struct FruitSlider: View {
+    var fruits: [Fruit] = fruitsData
+    var body: some View {
+        HStack{
+            TabView{
+                ForEach(fruits.prefix(5)) { fruit in
+                    FruitSlide(fruit: fruit)
+                        .padding(.horizontal, 10)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    FruitSlider()
+}
