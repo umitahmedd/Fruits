f2e7533ab6f9d3a4462592631f520cf59777e533
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-07 14:17:08 +0300

    Set to show FruitSlider view

--- Fruits/ContentView.swift
+++ Fruits/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("showFirstPage") var showFirstPage = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            if showFirstPage {
+                FruitSlider()
+            }
+            else
+            {
+                Home()
+            }
         }
-        .padding()
     }
 }
 
