d55d8c0edc40ac1a37dd33c49afe03fe4d5ed036
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-07 14:45:27 +0300

    Create FruitList component

---
+++ Fruits/Component/Common/FruitList.swift
@@ -0,0 +1,51 @@
+//
+//  FruitList.swift
+//  Fruits
+//
+//  Created by mac on 7.10.2023.
+//
+
+import SwiftUI
+
+struct FruitList: View {
+    var fruit:Fruit
+    var body: some View {
+        HStack(spacing:20){
+            Image("\(fruit.image)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .background(
+                    LinearGradient(
+                        gradient:
+                            Gradient(colors: [fruit.gradientColors[0], fruit.gradientColors[1]]),
+                        startPoint:.top, endPoint:.bottom
+                    )
+                )
+                .cornerRadius(10)
+            
+            
+            VStack(alignment: .leading){
+                Text("\(fruit.title)")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Spacer().frame(height: 5)
+                
+                Text("\(fruit.headline)")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.leading)
+            }
+            
+            Spacer()
+
+            
+            Image(systemName: "chevron.right")
+        }
+    }
+}
+
+#Preview {
+    FruitList(fruit: fruitsData[0])
+}
