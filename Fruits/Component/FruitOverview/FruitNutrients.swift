33f3f61b54f856f812963f73601d4d96089018a5
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-09 20:08:22 +0300

    Create FruitNutrients component

---
+++ Fruits/Component/FruitOverview/FruitNutrients.swift
@@ -0,0 +1,40 @@
+//
+//  FruitNutrients.swift
+//  Fruits
+//
+//  Created by mac on 7.10.2023.
+//
+
+import SwiftUI
+
+struct FruitNutrients: View {
+    var nutrietns = ["Energy","Sugar","Fat","Protein","Vitamins","Minerals"]
+    var fruit: Fruit
+    var body: some View {
+        GroupBox(){
+            DisclosureGroup(" Nutritional value per 100g"){
+                ForEach(0..<nutrietns.count, id:\.self){ item in
+                    Divider().padding(.vertical, 4)
+                    HStack{
+                        Group{
+                            Image(systemName: "info.circle")
+                            Text("\(nutrietns[item])")
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(.system(size: 16, weight: .bold))
+                        
+                        Spacer(minLength: 25)
+                        
+                        Text("\(fruit.nutrition[item])")
+                            .multilineTextAlignment(.trailing)
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    FruitNutrients(fruit: fruitsData[0])
+}
