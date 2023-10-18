ad93f669721ab90175217497d68b65fc4d718c4c
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-10-18 22:18:06 +0300

    Add app restart toggle

--- Fruits/View/SettingsSheet.swift
+++ Fruits/View/SettingsSheet.swift
@@ -9,67 +9,90 @@ import SwiftUI
 
 
 struct SettingsSheet: View {
-   @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-   var settingsRows: [SettingsRowView] = [
-      SettingsRowView(name: "Developer", content: "Ümit Ahmed"),
-      SettingsRowView(name: "Designer", content: "Ümit Ahmed"),
-      SettingsRowView(name: "Compatibility", content: "IOS 16.2"),
-      SettingsRowView(name: "Website", content: "umitahmed.com", url: "https://umitahmed.com"),
-      SettingsRowView(name: "Version", content: "1.1.0"),
-   ]
-   var body: some View {
-      NavigationView{
-         ScrollView(.vertical, showsIndicators: false){
-            VStack(spacing: 20){
-              
-            // MARK: HEAD
-               GroupBox {
-                  Divider( ).padding(.vertical, 4)
-                  HStack(spacing: 20){
-                     Image("logo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 80)
-                        .cornerRadius(10)
-                     
-                     
-                     Text("Most fruits are naturally low and bad, sodium and calories. None of cholesterol fruits are source of many essential, nutrients, including potassium, the entry, fiber, vitamins, and much more")
-                        .font(.footnote)
-                  }
-               } label: {
-                  SettingsLabelView(labelText: "Fruits", labelIcon: "info.circle")
-               }
-               
-              // MARK: APPLICATION INFO
-               GroupBox {
-                  ForEach(settingsRows, id:\.name){ row in
-                     Divider( ).padding(.vertical, 4)
-                     SettingsRowView(name: row.name, content: row.content, url: row.url)
-                  }
-                  
-               } label: {
-                  SettingsLabelView(labelText: "Application", labelIcon: "apps.iphone")
-               }
-            }
-            .toolbar{
-              // CLOSE BUTTON
-               ToolbarItem(placement: .topBarTrailing) {
-                  Button{
-                     presentationMode.wrappedValue.dismiss()
-                  }label: {
-                     Image(systemName: "xmark")
-                        .fontWeight(.medium)
-                  }
-               }
-            }
-            .navigationTitle("Settings")
-            .padding()
-         }
-      }
+  @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+  var settingsRows: [SettingsRowView] = [
+    SettingsRowView(name: "Developer", content: "Ümit Ahmed"),
+    SettingsRowView(name: "Designer", content: "Ümit Ahmed"),
+    SettingsRowView(name: "Compatibility", content: "IOS 16.2"),
+    SettingsRowView(name: "Website", content: "umitahmed.com", url: "https://umitahmed.com"),
+    SettingsRowView(name: "Version", content: "1.1.0"),
+  ]
+  @AppStorage("showFirstPage") var showFirstPage = true
 
-   }
+  var body: some View {
+    NavigationView{
+      ScrollView(.vertical, showsIndicators: false){
+        VStack(spacing: 20){
+          
+          // MARK: HEAD
+          GroupBox {
+            Divider( ).padding(.vertical, 4)
+            HStack(spacing: 20){
+              Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80)
+                .cornerRadius(10)
+              
+              
+              Text("Most fruits are naturally low and bad, sodium and calories. None of cholesterol fruits are source of many essential, nutrients, including potassium, the entry, fiber, vitamins, and much more")
+                .font(.footnote)
+            }
+          } label: {
+            SettingsLabelView(labelText: "Fruits", labelIcon: "info.circle")
+          }
+          
+          // MARK: Restart App
+          GroupBox {
+            Divider( ).padding(.vertical, 4)
+            
+            Text("Most fruits are naturally low and bad, sodium and calories. None of cholesterol fruits are source of many essential, nutrients, including potassium, the entry, fiber, vitamins, and much more")
+              .font(.footnote)
+              .padding(.vertical, 8)
+            
+            Toggle(isOn:$showFirstPage , label: {
+              Text("RESTART")
+            })
+            .padding()
+            .background(Color(UIColor.tertiarySystemBackground)
+              .clipShape(RoundedRectangle(cornerRadius: 8, style:.continuous ))
+            )
+            
+            
+          } label: {
+            SettingsLabelView(labelText: "Customization", labelIcon: "paintbrush")
+          }
+          
+          // MARK: APPLICATION INFO
+          GroupBox {
+            ForEach(settingsRows, id:\.name){ row in
+              Divider( ).padding(.vertical, 4)
+              SettingsRowView(name: row.name, content: row.content, url: row.url)
+            }
+            
+          } label: {
+            SettingsLabelView(labelText: "Application", labelIcon: "apps.iphone")
+          }
+        }
+        .toolbar{
+          // CLOSE BUTTON
+          ToolbarItem(placement: .topBarTrailing) {
+            Button{
+              presentationMode.wrappedValue.dismiss()
+            }label: {
+              Image(systemName: "xmark")
+                .fontWeight(.medium)
+            }
+          }
+        }
+        .navigationTitle("Settings")
+        .padding()
+      }
+    }
+    
+  }
 }
 
 #Preview {
-   SettingsSheet()
+  SettingsSheet()
 }
